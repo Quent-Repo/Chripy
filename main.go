9eb7068922f5ad0668607918939b96cb1d723106
owner <grrkekisbest@gmail.com>
2024-02-26 11:39:24 -0500

    Test

--- main.go
+++ main.go
@@ -4,31 +4,6 @@ import (
 	"net/http"
 )
 
-// type Page struct {
-// 	Title string
-// 	Body  []byte
-// }
-
-// func (p *Page) save() error {
-// 	filename := p.Title + ".txt"
-// 	return os.WriteFile(filename, p.Body, 0600)
-// }
-
-// func loadPage(title string) (*Page, error) {
-// 	filename := title + ".txt"
-// 	body, err := os.ReadFile(filename)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &Page{Title: title, Body: body}, nil
-// }
-
-// func viewHandler(w http.ResponseWriter, r *http.Request) {
-// 	title := r.URL.Path[len("/view/"):]
-// 	p, _ := loadPage(title)
-// 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-// }
-
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -46,13 +21,8 @@ func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
 	r := http.Server{
+		Addr:    ":8080",
 		Handler: corsMux,
 	}
 	r.ListenAndServe()
-	// http.HandleFunc("/view/", viewHandler)
-	// log.Fatal(http.ListenAndServe(":8080", nil))
-	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page")}
-	// p1.save()
-	// p2, _ := loadPage("TestPage")
-	// fmt.Println(string(p2.Body))
 }
