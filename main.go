618fbdfc5b47476873ce672c6bb6a6b8659a7f29
owner <grrkekisbest@gmail.com>
2024-02-26 11:32:08 -0500

    Test

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+)
+
+// type Page struct {
+// 	Title string
+// 	Body  []byte
+// }
+
+// func (p *Page) save() error {
+// 	filename := p.Title + ".txt"
+// 	return os.WriteFile(filename, p.Body, 0600)
+// }
+
+// func loadPage(title string) (*Page, error) {
+// 	filename := title + ".txt"
+// 	body, err := os.ReadFile(filename)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &Page{Title: title, Body: body}, nil
+// }
+
+// func viewHandler(w http.ResponseWriter, r *http.Request) {
+// 	title := r.URL.Path[len("/view/"):]
+// 	p, _ := loadPage(title)
+// 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+// }
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+	r := http.Server{
+		Handler: corsMux,
+	}
+	r.ListenAndServe()
+	// http.HandleFunc("/view/", viewHandler)
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+}
